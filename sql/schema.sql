ef8cf1975e621074cb4786a49ac82dec8dab7859
Timothy Ehli <tehli1882@gmail.com>
2025-02-03 12:01:54 -0500

    inital set up and inquire page

---
+++ sql/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+
+\c tracker_db
+
+CREATE TABLE department (id SERIAL PRIMARY KEY, name VARCHAR(30));
+CREATE TABLE role (id SERIAL PRIMARY KEY, title VARCHAR(30), salary DECIMAL, department_id INT);
+CREATE TABLE employee (id SERIAL PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT, manager_id INT);
